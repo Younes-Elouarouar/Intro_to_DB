3c3b31e6c850ed12aacf2e7cdf497b545aba17de
Younes-Elouarouar <elouarouaryounes1@gmail.com>
2024-12-01 13:31:47 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 
 );
 
